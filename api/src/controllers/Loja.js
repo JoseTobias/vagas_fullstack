24fd3dbd396610188475b2ca0e2fd3c194274790
JoseTobias <tobiassouza0@gmail.com>
2020-03-13 20:17:10 -0300

    Fazendo controller de loja

---
+++ api/src/controllers/Loja.js
@@ -0,0 +1,42 @@
+const Store = require('../models/Loja');
+
+
+exports.store_create = async (req, res) => {
+    const { name, logo, link } = req.body;
+
+
+    const storeExists = await Store.findOne({ user: name });
+
+    if(storeExists) {
+        return res.json(storeExists);
+    }
+
+    const store = await Store.create({
+        name,
+        logo,
+        link
+    })
+    return res.json(store);
+}
+
+exports.store_get = async (req, res) => {
+    const stores = await Store.find()
+    return res.json(stores);
+}
+
+exports.store_update = async (req, res, next) => {
+    const _id = req.params.storeId
+    const { name, logo, link } = req.body
+    const store = await Store.updateOne({_id}, {
+        name,
+        logo,
+        link
+    })
+    return res.json(store)
+}
+
+exports.store_delete = async (req, res, next) => {
+    const _id = req.params.storeId
+    const stores = await Store.deleteOne({_id})
+    return res.json(stores)
+}
