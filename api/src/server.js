e75cb0577676ddee76542003f8c612c112c82bb0
JoseTobias <tobiassouza0@gmail.com>
2020-03-17 20:50:37 -0300

    Colocando cros

--- api/src/server.js
+++ api/src/server.js
@@ -4,9 +4,13 @@ const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
+const allowCors = require('./config/cors')
+
 require('dotenv').config();
 
 const app = express();
+
+app.use(allowCors);
 
 const server = require('http').Server(app);
 
